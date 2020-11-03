1b500f57fd49e495d7580aea64138a927966384b
jnunes-wtf <58940794+jnunes-wtf@users.noreply.github.com>
2020-11-02 22:50:10 -0300

    Feito consumo da API do Back-end
    Feita a instalação do AXIOS para o consumo da api do back-end. Agora os comentários ficam salvos dentro do banco de dados.

--- src/components/Coments.vue
+++ src/components/Coments.vue
@@ -17,21 +17,24 @@
     >
       Comentar
     </button>
+    <hr />
+    <!-- {{comentarios}} -->
     <ul class="list-group">
       <div
         class="list-group-item"
         v-bind:key="index"
-        v-for="(comentario, index) in todosComentarios"
+        v-for="(comentario, index) in comentarios"
       >
-        <span class="coment__autor">Autor: {{ comentario.nome }}</span>
-        <p>Mensagem: {{ comentario.mensagem }}</p>
-        <a href="#" v-on:click.prevent="removerComentario(index)">Excluir</a>
+        <span class="coment__autor">Autor: {{ comentario.nome}}</span>
+        <p>Mensagem: {{ comentario.mensagem}}</p>
+        <a href="#" v-on:click.prevent="removerComentario(comentario.id)">Excluir</a>
       </div>
     </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   data() {
     return {
@@ -39,9 +42,10 @@ export default {
       * Array de comentários
       * Autor: José Carlos Júnior
       */
-      comentarios: [],
+      // comentarios: [],
       nome: "",
       mensagem: "",
+      comentarios: '',
     };
   },
   methods: {
@@ -50,14 +54,20 @@ export default {
      * Autor: José Carlos Júnior
      */
     adicionarComentario() {
-      if (this.mensagem.trim() === "") {
-        alert("O campo mensagem não pode estar em branco!");
-        return;
-      }
-      this.comentarios.push({
-        nome: this.nome,
-        mensagem: this.mensagem,
-      });
+      // if (this.mensagem.trim() === "") {
+      //   alert("O campo mensagem não pode estar em branco!");
+      //   return;
+      // }
+      axios
+        .post('http://127.0.0.1:8000/api/comentarios/',{'nome':this.nome,'mensagem':this.mensagem})
+        .then(response => console.log(response))
+        location.reload();
+        
+      
+      // this.comentarios.push({
+      //   nome: this.nome,
+      //   mensagem: this.mensagem,
+      // });
 
       this.nome = "";
       this.mensagem = "";
@@ -66,9 +76,19 @@ export default {
      * Função para remover comentários
      * Autor: José Carlos Júnior
      */
-    removerComentario(index) {
-      this.comentarios.splice(index, 1);
+    removerComentario(id) {
+      console.log(id);
+      axios 
+        .delete('http://127.0.0.1:8000/api/comentarios/'+id)
+        .then(response => (console.log(response.data),location.reload()))
+      // this.comentarios.splice(index, 1);
     },
+  },
+  mounted (){
+    axios
+      .get('http://127.0.0.1:8000/api/')
+      .then(response => (this.comentarios = response.data))
+      
   },
   computed: {
     /**
